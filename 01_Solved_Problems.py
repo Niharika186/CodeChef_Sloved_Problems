0c34f6a77e1fab4ca21b7dd294bdd6486b2b606a
Niharika186 <107666477+Niharika186@users.noreply.github.com>
2022-07-13 19:03:57 +0530

    Add files via upload

---
+++ 01_Solved_Problems.py
@@ -0,0 +1,62 @@
+# First Problem #
+'''Chef has to travel to another place. For this, he can avail any one of two cab services.'''
+
+'''The first cab service charges XX rupees.
+The second cab service charges YY rupees.
+Chef wants to spend the minimum amount of money. Which cab service should Chef take?
+
+Input Format :
+The first line will contain TT - the number of test cases. Then the test cases follow.
+The first and only line of each test case contains two integers XX and YY - the prices of first and second cab services respectively.'''
+
+# cook your dish here
+# T=int(input('Input values: '))
+# for i in range(T):
+#     x,y=[int(a) for a in input().split()]
+#     if x<y:
+#         print('First')
+#     elif x>y:
+#         print('Second')
+#     elif x==y:
+#         print('Any')
+
+
+### Second Problem ###
+'''Problem
+Alice and Bob were having an argument about which of them is taller than the other. Charlie got irritated by the argument, and decided to settle the matter once and for all.
+
+Charlie measured the heights of Alice and Bob, and got to know that Alice's height is XX centimeters and Bob's height is YY centimeters. Help Charlie decide who is taller.
+
+It is guaranteed that X \neq YX=Y.
+
+Input Format: 
+The first line of input will contain an integer TT — the number of test cases. The description of TT test cases follows.
+The first and only line of each test case contains two integers XX and YY, as described in the problem statement.'''
+
+# cook your dish here
+# T=int(input())
+# for n in range(T):
+#     A,B=[int(x) for x in input().split()]
+#     if A<B:
+#         print('B')
+#     elif A>B:
+#         print('A')
+
+
+### Third Problem ###
+'''Problem
+Chef is eagerly waiting for a piece of information. His secret agent told him that this information would be revealed to him after KK weeks.
+
+XX days have already passed and Chef is getting restless now. Find the number of remaining days Chef has to wait for, to get the information.
+
+It is guaranteed that the information has not been revealed to the Chef yet.
+
+Input Format:
+The first line of input will contain an integer TT — the number of test cases. The description of TT test cases follows.
+The first and only line of each test case contains two space-separated integers KK and XX, as described in the problem statement.'''
+
+# cook your dish here
+# T=int(input())
+# for n in range(T):
+#     k,x=[int(a) for a in input().split()]
+#     print((k*7)-x)
